fc9a81312c95371a8c7e7cdafefae37c55b1945c
Anita <smirnovasiberia@gmail.com>
2019-03-24 19:10:03 +0300

    sets operations

--- dz_2.c
+++ dz_2.c
@@ -51,13 +51,6 @@ char **allocateTokens(int line_length) {
     exit(EXIT_FAILURE);
   }
   return tokens;
-//  for (int i = 0; i < line_length; i++) {
-//    tokens[i] = malloc(line_length * sizeof(char));
-//    if (tokens[i] == NULL) {
-//      perror("malloc");
-//      exit(EXIT_FAILURE);
-//    }
-//  }
 }
 
 void free_memory(char **strings, int strings_capacity) {
@@ -65,47 +58,148 @@ void free_memory(char **strings, int strings_capacity) {
     free(strings[i]);
   free(strings);
 }
+int compare( const void* a, const void* b)
+{
+  int int_a = * ( (int*) a );
+  int int_b = * ( (int*) b );
 
-void createToken(char ** tokens, int token_number, int token_first_pos, int token_last_pos, char *line) {
+  if ( int_a == int_b ) return 0;
+  if ( int_a < int_b ) return -1;
+  return 1;
+}
+
+void convertStrtoArr(const char *str) {
+  // 128 max length
+  int arr[128] = {0};
+  int flag_digit = 0;
+  int j = -1;
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (str[i] == ',' || isspace(str[i])) {
+      flag_digit = 0;
+      // j++;
+    } else {
+      if (flag_digit == 0) {
+        flag_digit = 1;
+        j++;
+      }
+      arr[j] = arr[j] * 10 + (str[i] - 48);
+    }
+  }
+  size_t arr_length = (size_t) j + 1;
+  if (j != -1) {
+    qsort(arr, arr_length, sizeof(int), compare);
+  }
+  for (int i = 0; i <= j; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
+}
+
+/* Function prints union of arr1[] and arr2[]
+   m is the number of elements in arr1[]
+   n is the number of elements in arr2[] */
+
+int getUnion(const int *arr1, const int *arr2, int m, int n, int *my_union)
+{
+  int union_elements  = 0;
+
+  int i = 0, j = 0;
+  while (i < m && j < n)
+  {
+    if (arr1[i] < arr2[j]) {
+      my_union[union_elements] = arr1[i++];
+      union_elements++;
+    }
+    else if (arr2[j] < arr1[i]) {
+      my_union[union_elements] = arr2[j++];
+      union_elements++;
+    }
+    else
+    {
+      my_union[union_elements] = arr2[j++];
+      union_elements++;
+      i++;
+    }
+  }
+  /* Print remaining elements of the larger array */
+  while(i < m) {
+    my_union[union_elements] = arr1[i++];
+    union_elements++;
+  }
+  while(j < n){
+    my_union[union_elements] = arr2[j++];
+    union_elements++;
+  }
+  return union_elements;
+}
+
+/* Function prints Intersection of arr1[] and arr2[]
+   m is the number of elements in arr1[]
+   n is the number of elements in arr2[] */
+int getIntersection(const int *arr1, int *arr2, int m, int n, int *my_intersection)
+{
+  int i = 0, j = 0;
+  int intersections_elements = 0;
+  while (i < m && j < n)
+  {
+    if (arr1[i] < arr2[j])
+      i++;
+    else if (arr2[j] < arr1[i])
+      j++;
+    else /* if arr1[i] == arr2[j] */
+    {
+      my_intersection[intersections_elements] = arr2[j++];
+      intersections_elements++;
+      i++;
+    }
+  }
+  return intersections_elements;
+}
+
+void createToken(char **tokens, int token_number, int token_first_pos, int token_last_pos, char *line) {
   int token_length = token_last_pos - token_first_pos;
   tokens[token_number] = malloc(token_length * sizeof(char));
   if (tokens[token_number] == NULL) {
     perror("malloc");
     exit(EXIT_FAILURE);
   }
-  strncpy(tokens[token_number], line + token_first_pos, token_length);
+  strncpy(tokens[token_number], line + token_first_pos, (size_t) token_length);
 }
+
 void findTokens(char *line, int line_length) {
-  int digit_token_flag = -1;
+  int digit_token_flag = 0;
   char **tokens = allocateTokens(line_length);
+  int digit_first_pos = 0;
+  int digit_last_pos = 0;
 
   int tokens_amount = 0;
   for (int i = 0; i < line_length; i++) {
     if (!isspace(line[i])) {
       if (line[i] == '[') {
         digit_token_flag = 1;
+        digit_first_pos = i + 1;
       } else if (line[i] == ']') {
-        //printf("\n");
         digit_token_flag = 0;
+        digit_last_pos = i;
+        createToken(tokens, tokens_amount, digit_first_pos, digit_last_pos, line);
+        tokens_amount++;
       } else {
-        if (digit_token_flag == -1) {
-          digit_token_flag = 0;
-        }
         if (digit_token_flag == 0) {
-          createToken(tokens, tokens_amount, i, i+1, line);
+          createToken(tokens, tokens_amount, i, i + 1, line);
           tokens_amount++;
-          //printf("%c\n", line[i]);
-        } else {
-          //printf("%c", line[i]);
         }
       }
     }
   }
   for (int i = 0; i < tokens_amount; i++) {
-    printf("%s\n", tokens[i]);
+    if (tokens[i][0] != 'U' && tokens[i][0] != '^' && tokens[i][0] != '(' && tokens[i][0] != ')')
+      convertStrtoArr(tokens[i]);
+    else
+      printf("%s\n", tokens[i]);
   }
-  printf("\n");
-  free_memory(tokens, tokens_amount);
+  printf("NEW\n");
+  //free_memory(tokens, tokens_amount);
 }
 
 int main() {
@@ -120,9 +214,27 @@ int main() {
   ssize_t line_length;
 
   while ((line_length = getline(&line, &len, stream)) != -1) {
-   // printf("Retrieved line of length %zu:\n", line_length);
+    // printf("Retrieved line of length %zu:\n", line_length);
     fwrite(line, line_length, 1, stdout);
     findTokens(line, line_length);
+  }
+
+  int arr1[] = {1, 3, 3, 5, 6, 6, 8, 9, 70 };
+  int arr2[] = {-29, 1, 3, 5, 6, 7, 9, 13, 15};
+
+  int m = sizeof(arr1)/sizeof(arr1[0]);
+  int n = sizeof(arr2)/sizeof(arr2[0]);
+
+  printf("INTERSEC\n");
+
+  int my_union[128] = {0};
+  int union_elements = getUnion(arr1, arr2, m, n, my_union);
+
+  int my_intersections[128] = {0};
+  int intersections_elements = getIntersection(arr1, arr2, m, n, my_intersections);
+
+  for(int i = 0; i < intersections_elements; i++) {
+    printf("%d ", my_intersections[i]);
   }
 
   free(line);
